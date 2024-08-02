2684a85b4c536ff959d635f16668920d1d9d5a60
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-08-03 03:07:30 +0700

    asdasda

--- js/home.js
+++ js/home.js
@@ -1,39 +1,31 @@
 /*----------------------------category*---------------/ */
 function openCategory(evt, categoryname) {
-    slideIndex = 1;
-    showSlides(slideIndex);
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(categoryname).style.display = "flex";
-    evt.currentTarget.className += " active";
-    // document.getElementById("defaultOpen").click();
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(categoryname).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+document.getElementById("defaultOpen").click();
 
 
 /*-------------------------------slider tab----------------------------*/
 
 let slideIndex = 1;
-let count = slideIndex;
 showSlides(slideIndex);
 function plusSlides(n) {
-    count = count + n;
-    if (count > 3) {
-      slideIndex = 0;
-      count = 1;  
-    }
-    showSlides(slideIndex += n);
-    console.log(slideIndex);
+  showSlides(slideIndex += n);
+  console.log(slideIndex);
 }
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 function showSlides(n) {
   let i;
