f496b5f1182ac0e6f71477993c41bc76f8b0946e
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-08-02 13:11:25 +0700

    go

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
+/*----------------------------category*---------------/ */
 function openCategory(evt, categoryname) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
+    
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
@@ -10,4 +12,27 @@ function openCategory(evt, categoryname) {
     }
     document.getElementById(categoryname).style.display = "flex";
     evt.currentTarget.className += " active";
+}
+
+
+/*-------------------------------slider tab----------------------------*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex"; 
 }
