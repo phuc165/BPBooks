bbb36c83f904d7f41c37fc4082384e0923bf84f2
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-08-04 11:01:09 +0700

    kjmjm

--- js/home.js
+++ js/home.js
@@ -1,39 +1,53 @@
-/*----------------------------category*---------------/ */
-function openCategory(evt, categoryname) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(categoryname).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-document.getElementById("defaultOpen").click();
-
-
 /*-------------------------------slider tab----------------------------*/
 
 let slideIndex = 1;
-showSlides(slideIndex);
 function plusSlides(n) {
+  if (n > 3) {
+    n = 1;
+  }
   showSlides(slideIndex += n);
   console.log(slideIndex);
 }
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-function showSlides(n) {
+ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+  slideIndex=n
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "flex"; 
+  slides[n].style.display = "flex"; 
+
 }
+/*----------------------------category*---------------/ */
+
+function openCategory(evt, categoryname,slideIndx) {
+ 
+  var  tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+
+showSlides(slideIndx);
+
+  for ( var i = 0; i < tabcontent.length; i++) {
+    
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  var elemet = document.getElementById(categoryname)
+ 
+  elemet.style.display = "block"; 
+  evt.currentTarget.className += " active";
+  
+}
+document.getElementById("defaultOpen").click();
+
+
+ 
