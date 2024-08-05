90f65c7e8e667eff0bddb4267db5b17baa6d69a0
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-08-05 16:32:14 +0700

    body slider fix

--- js/home.js
+++ js/home.js
@@ -1,20 +1,23 @@
 /*-------------------------------slider tab----------------------------*/
 
-let slideIndex = 1;
+let slideIndex = 0;
+let count = 0;
 function plusSlides(n) {
-  if (n > 3) {
-    n = 1;
-  }
+  count = count + n;
   showSlides(slideIndex += n);
-  console.log(slideIndex);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+  // console.log(slideIndex);
+  console.log(count);
+  // if (count > 0 && count < 2){
+  //   document.getElementById("slider_next").disabled = false;
+  //   document.getElementById("slider_prev").disabled = false;
+  //   console.log('undis')
+  // }
+  // if (count <= 0) {document.getElementById("slider_prev").disabled = true; console.log('dis');}
+  // if (count >= 2){document.getElementById("slider_next").disabled = true; console.log('dis');}
 }
  function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   slideIndex=n
@@ -27,14 +30,11 @@ function currentSlide(n) {
 /*----------------------------category*---------------/ */
 
 function openCategory(evt, categoryname,slideIndx) {
- 
+  count = 0;
   var  tabcontent, tablinks;
-tabcontent = document.getElementsByClassName("tabcontent");
-
-showSlides(slideIndx);
-
+  tabcontent = document.getElementsByClassName("tabcontent");
+  showSlides(slideIndx);
   for ( var i = 0; i < tabcontent.length; i++) {
-    
     tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
@@ -42,10 +42,8 @@ showSlides(slideIndx);
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   var elemet = document.getElementById(categoryname)
- 
   elemet.style.display = "block"; 
   evt.currentTarget.className += " active";
-  
 }
 document.getElementById("defaultOpen").click();
 
