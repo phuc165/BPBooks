3cfb70c383e5d49886b8afd9ae3775c9a4aab3ae
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-09-03 05:06:57 +0700

    Delete hon directory

--- hon/ASM_9_Phan Việt Hoàng/DoAN/admin/function/deleteBill.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php
-    require_once 'functions.php';
-    $id = $_GET['billID'];
-    settype($id, "int");
-    deleteProduct($id);
-    header("location:  ../admin.php");
--- hon/ASM_9_Phan Việt Hoàng/DoAN/js/header.js
+++
@@ -1,9 +0,0 @@
-var userInfomation=document.getElementById("user-information")
-
-        function ShowInfomation(){
-          if(  userInfomation.classList.contains("hidden")){
-            userInfomation.classList.remove("hidden")
-          }else{
-            userInfomation.classList.add("hidden")
-          }
-        }
